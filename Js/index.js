82a89b718c0bd8b4a4236497b2824b446c8e702e
Maximilian Rau <st188391@stud.uni-stuttgart.de>
2025-03-26 18:41:44 +0100

    some js

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,5 @@
+// counter
+
 const incrementBtn = document.getElementById('incrementButton');
 const decrementBtn = document.getElementById('decrementButton');
 const resetBtn = document.getElementById('resetButton');
@@ -17,4 +19,49 @@ decrementBtn.onclick = function() {
 resetBtn.onclick = function() {
     count = 0;
     countLabel.textContent = count;
+}
+
+// random number generator
+
+const myButton = document.getElementById('myButton');
+const label1 = document.getElementById('label1');
+const label2 = document.getElementById('label2');
+const label3 = document.getElementById('label3');
+const min = 1;
+const max = 6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function() {
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
+}
+
+// age checker
+
+const myText = document.getElementById('myText');
+const mySubmit = document.getElementById('mySubmit');
+const resultElement = document.getElementById('resultElement');
+let age;
+
+mySubmit.onclick = function() {
+    age = myText.value;
+    age = Number(age);
+    if (age >= 100) {
+        resultElement.textContent = 'You are too old.';
+    } else if (age >= 18) {
+        resultElement.textContent = 'You are an adult';
+    } else if (age == 0) {
+        resultElement.textContent = 'You were just born';
+    } else if (age < 0) {
+        resultElement.textContent = 'You are not born yet';
+    }
+    else {
+        resultElement.textContent = 'You are a child';
+    }
 }
