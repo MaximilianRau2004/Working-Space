fe5f2c70ee5613cead51175aa784ff6c8560dba5
MaximilianRau2004 <maxi.rau2004@gmail.com>
2025-06-07 14:29:22 +0200

    delet some files

--- Rust/target/debug/deps/main-4bae9a56283fbb02.d
+++
@@ -1,5 +0,0 @@
-/home/maxi/Dokumente/GitHub/Working-Space/Rust/main/target/debug/deps/main-4bae9a56283fbb02: src/main.rs
-
-/home/maxi/Dokumente/GitHub/Working-Space/Rust/main/target/debug/deps/main-4bae9a56283fbb02.d: src/main.rs
-
-src/main.rs:
--- Rust/target/debug/main.d
+++
@@ -1,1 +0,0 @@
-/home/maxi/Dokumente/GitHub/Working-Space/Rust/main/target/debug/main: /home/maxi/Dokumente/GitHub/Working-Space/Rust/main/src/main.rs
