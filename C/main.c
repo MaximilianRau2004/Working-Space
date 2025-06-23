295080761dac0b766c064e18ba1bc52e76bd88e7
MaximilianRau2004 <maxi.rau2004@gmail.com>
2025-06-23 19:18:05 +0200

    c

--- C/main.c
+++ C/main.c
@@ -1,25 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
-void birthday(int* age);
 
 int main() {
- 
-    int age = 25;
-    int *pAge = &age;
 
-    birthday(pAge);
-    printf("Age: %d\n", age);
+    /** 
+    int number = 0;
+    printf("Enter the number of grades: ");
+    scanf("%d", &number);
 
-    printf("%p\n", &age); 
+    char *grades = malloc(number * sizeof(char));
 
-    printf("%p\n", pAge);
+    if (grades == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1; 
+    }
+
+    for(int i = 0; i < number; i++) {
+        printf("Enter grade %d: ", i + 1);
+        scanf(" %c", &grades[i]); 
+    }
+
+    for(int i = 0; i < number; i++) {
+        printf("Grade %d: %c\n", i + 1, grades[i]);
+    }
+
+    free(grades); // deallocate memory
+    grades = NULL; // avoids dangling pointer
+    */
+
+    int number = 0;
+    printf("Enter the number of players: ");
+    scanf("%d", &number);
+
+    int *scores = calloc(number, sizeof(int));
+
+    if(scores == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1; 
+    }
+
+    for(int i = 0; i < number; i++) {
+        printf("Enter score for player %d: ", i + 1);
+        scanf("%d", &scores[i]);
+    }
+
+    for(int i = 0; i < number; i++) {
+        printf("Score for player %d: %d\n", i + 1, scores[i]);
+    }
+
+    free(scores);
+    scores = NULL;
 
     return 0;
 }
 
-void birthday(int* age) {
-    (*age)++;
-}
+
  
