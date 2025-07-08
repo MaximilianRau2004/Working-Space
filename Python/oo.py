f954f2b35c8e0d96b8dfaa2a20ff41900a5a9985
MaximilianRau2004 <maxi.rau2004@gmail.com>
2025-07-08 22:02:30 +0200

    py oop

--- Python/oo.py
+++ Python/oo.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 
 class Car:
     def __init__(self, model, color, year, for_sale):
@@ -26,3 +27,56 @@ print(car1)
 car1.drive()
 car1.stop()
 car1.sell()
+
+
+class Animal:
+    def __init__(self, name, color, age):
+        self.name = name
+        self.color = color
+        self.age = age
+
+    @abstractmethod
+    def eat(self):
+        pass
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+    def walk(self):
+        print(f"{self.name} is walking")
+
+class Pet(Animal):
+    def __init__(self, owner, name, color, age):
+        super().__init__(name, color, age)
+        self.owner = owner
+
+    def pet(self):
+        print(f"{self.name} is being petted")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+class Dog(Pet):
+    def bark(self):
+        print(f"{self.name} is barking")
+
+    def walk(self):
+        super().walk()
+
+class Cat(Pet):
+    def meow(self):
+        print(f"{self.name} is meowing")
+
+class Hawk(Animal):
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+dog = Dog("Rex", "brown", 3, "John")
+cat = Cat("Fluffy", "white", 2, "Jane")
+hawk = Hawk("Bully", "black", 1)
+
+dog.bark()
+dog.walk()
+dog.pet()
+
+animals = [dog, cat, hawk]
