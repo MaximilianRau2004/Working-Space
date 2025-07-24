e0bb4cd8d77f0803666b828c3f26c4a5dc4fcb62
MaximilanRau04 <maxi.rau2004@gmail.com>
2025-07-24 16:08:07 +0200

    alarm in py

--- Python/main.py
+++ Python/main.py
@@ -1,3 +1,20 @@
+import datetime
+
+date = datetime.date(2025, 1, 2)
+today = datetime.date.today()
+time = datetime.time(hour=23, minute=59, second=59)
+now = datetime.datetime.now()
+
+now = now.strftime("%H:%M:%S %d-%m-%y")
+
+target_datetime = datetime.datetime(2026, 1, 2, 12, 30, 1)
+current_datetime = datetime.datetime.now()
+
+if current_datetime < target_datetime:
+    print("Target date is in the future")
+else:
+    print("Target date is in the past")
+
 def add_sprinkles(func):
     def wrapper():
         func()
