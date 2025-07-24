c4850686ae951833bbd3b967c8e5b207f3f3145d
MaximilanRau04 <maxi.rau2004@gmail.com>
2025-07-24 19:52:45 +0200

    dom js

--- Js/index.js
+++ Js/index.js
@@ -673,3 +673,37 @@ ulElements.forEach(ul => {
 const element2 = document.getElementById("carrots");
 const prevSibling = element2.previousElementSibling;
 prevSibling.style.color = "green";
+
+
+const newH1 = document.createElement("h1");
+
+newH1.textContent = "New Heading";
+newH1.id = "myH1";
+newH1.style.color = "tomato";
+newH1.style.textAlign = "center";
+
+// document.body.append(newH1);
+// document.body.prepend(newH1);
+// document.getElementById("box1").append(newH1);
+// document.getElementById("box1").prepend(newH1);
+
+// const box2 = document.getElementById("box2");
+// document.body.insertBefore(newH1, box2);
+
+// const boxes = document.querySelectorAll(".box");
+// document.body.insertBefore(newH1, boxes[0]);
+// document.getElementById("box1").removeChild(newH1);
+
+const newListItem = document.createElement("li");
+
+newListItem.textContent = "orange";
+newListItem.id = "orange";
+newListItem.style.fontWeight = "bold";
+newListItem.style.color = "orange";
+
+// document.getElementById("fruits").append(newListItem);
+// const orange = document.getElementById("orange");
+// document.getElementById("fruits").insertBefore(newListItem, orange);
+
+const listItems = document.querySelectorAll("#fruits li");
+document.getElementById("fruits").insertBefore(newListItem, listItems[1]);
